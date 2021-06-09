c1a610f6166b053a030fe5e769bb1f32bd926bca
DhanashriHabde <dhanu224454@gmail.com>
2021-06-09 16:54:25 +0530

    Added git ignore

--- pro14.py
+++ pro14.py
@@ -10,9 +10,11 @@ bottle = cv.cvtColor(bottle, cv.COLOR_BGR2RGB)
 bottle = cv.resize(bottle, (471, 671))
 
 # cv.imshow("Bottle",bottle)
+
 plt.imshow(bottle)
 plt.title("Original image")
 plt.show()
+
 
 # printing size
 '''
@@ -23,64 +25,64 @@ print("pixels:",bottle)
 '''
 # Grayscale_image
 red_frame = bottle[:, :, 0]
-cv.imshow("red frame", red_frame)
-cv.waitKey(0)
+#cv.imshow("red frame", red_frame)
+#cv.waitKey(0)
 
 # drawing histogram
 plt.hist(red_frame.ravel(), 256, [0, 256])
 plt.title("Histogram of redframe")
-plt.show()
+#plt.show()
 
 # eliminating background
 for row in range(red_frame.shape[0]):
     for col in range(red_frame.shape[1]):
         if red_frame[row, col] > 100:
             red_frame[row, col] = 1
-cv.imshow("background masked", red_frame)
+#cv.imshow("background masked", red_frame)
 cv.waitKey()
 
 for row in range(red_frame.shape[0]):
     for col in range(red_frame.shape[1]):
         if red_frame[row, col] < 100 and red_frame[row, col] != 1:
             red_frame[row, col] = 255
-cv.imshow("liquid masked", red_frame)
+#cv.imshow("liquid masked", red_frame)
 cv.waitKey()
 
 # smoothening operation
 bottle_smooth = cv.GaussianBlur(red_frame, (7, 7), 0)
-cv.imshow("Smooth bottle", bottle_smooth)
+#cv.imshow("Smooth bottle", bottle_smooth)
 cv.waitKey(0)
 
 # OTSU's Thresholding
 #ret1, th1 = cv.threshold(red_frame, 0, 255, cv.THRESH_BINARY)
 #ret2, th2 = cv.threshold(red_frame, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 ret3, th3 = cv.threshold(bottle_smooth, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-cv.imshow("thresholding", th3)
+#cv.imshow("thresholding", th3)
 #cv.imshow("thresholding2", th2)
 cv.waitKey()
 
 # dialation operation
 kernel = np.ones((9, 9), np.uint8)
 dilation = cv.dilate(th3, kernel, iterations=1)
-cv.imshow("dilation", dilation)
+#cv.imshow("dilation", dilation)
 cv.waitKey(0)
 
 # opening and closing operations
 opening = cv.morphologyEx(dilation, cv.MORPH_OPEN, kernel)
-cv.imshow("opening operation", opening)
+#cv.imshow("opening operation", opening)
 cv.waitKey(0)
 closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
-cv.imshow("closing  operation", closing)
+#cv.imshow("closing  operation", closing)
 cv.waitKey(0)
 
 # Erosion to reduce noise remaining
 erosion = cv.erode(closing, kernel, iterations=3)
-cv.imshow("erosion", erosion)
+#cv.imshow("erosion", erosion)
 cv.waitKey(0)
 
 # canny detection
 canny = cv.Canny(erosion, 100, 150)
-cv.imshow("canny", canny)
+#cv.imshow("canny", canny)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
